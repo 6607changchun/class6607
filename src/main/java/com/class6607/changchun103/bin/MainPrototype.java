5ec638cd8d954f498052af0cdafdda53f65fa473
changchun <luobojiaozi@163.com>
2021-05-21 17:56:10 +0800

    spring boot for git

---
+++ src/main/java/com/class6607/changchun103/bin/MainPrototype.java
@@ -0,0 +1,11 @@
+package com.class6607.changchun103.bin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainPrototype {
+    public static void main(String[] args) {
+        SpringApplication.run(MainPrototype.class,args);
+    }
+}
