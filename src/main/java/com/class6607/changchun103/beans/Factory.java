82a846eb13982d176483fd3599e2e7e0338680ee
changchun <luobojiaozi@163.com>
2021-06-01 22:25:16 +0800

    spring boot with database(add Serializable interface for domain objects)

--- src/main/java/com/class6607/changchun103/beans/Factory.java
+++ src/main/java/com/class6607/changchun103/beans/Factory.java
@@ -4,12 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Factory {
+public class Factory implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = -6300896953673101599L;
     private Integer fid;
     private String factoryName;
     private String factoryLocation;
--- src/main/java/com/class6607/changchun103/beans/Train.java
+++ src/main/java/com/class6607/changchun103/beans/Train.java
@@ -4,10 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Train {
+public class Train implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = -5479314192085296627L;
     private Integer tid;
     private String typeName;
     private Integer maxSpeed;
